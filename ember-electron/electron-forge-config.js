0a6d17374381a81f811c4ee8af0409f8570e7f1b
Devin Alexander Torres <d@devinus.io>
2017-12-18 10:08:22 -0600

    feat: Add ember-electron addon

---
+++ ember-electron/electron-forge-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "make_targets": {
+    "win32": [
+      "squirrel"
+    ],
+    "darwin": [
+      "zip"
+    ],
+    "linux": [
+      "deb",
+      "rpm"
+    ]
+  },
+  "electronPackagerConfig": {
+    "packageManager": "npm"
+  },
+  "electronWinstallerConfig": {
+    "name": "raiwallet"
+  },
+  "electronInstallerDebian": {},
+  "electronInstallerRedhat": {},
+  "github_repository": {
+    "owner": "",
+    "name": ""
+  },
+  "windowsStoreConfig": {
+    "packageName": "",
+    "name": "raiwallet"
+  }
+};
