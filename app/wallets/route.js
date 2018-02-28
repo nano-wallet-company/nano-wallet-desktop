5b7d79b7314f8c20d85ce4a0cd0df8f6c2da973f
makobi <alex.santos.sosa@gmail.com>
2018-02-27 21:05:05 -0400

    style(settings): style wallet settings

--- app/wallets/route.js
+++ app/wallets/route.js
@@ -2,12 +2,14 @@ import Route from '@ember/routing/route';
 import { get } from '@ember/object';
 import { tryInvoke } from '@ember/utils';
 import { action } from 'ember-decorators/object';
-
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import { service } from 'ember-decorators/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   @service electron: null,
+  @service rpc: null,
+  @service intl: null,
+  @service session: null,
 
   beforeModel() {
     const electron = this.get('electron');
@@ -35,5 +37,18 @@ export default Route.extend(AuthenticatedRouteMixin, {
     const message = this.get('intl').t('wallets.overview.created');
     this.get('flashMessages').success(message);
     return this.transitionTo('wallets.accounts', account);
+  },
+
+  @action
+  async changePassword(model, changeset) {
+    const session = this.get('session');
+    const wallet = get(model, 'id');
+    const password = get(changeset, 'password');
+    await this.get('rpc').passwordChange(wallet, password);
+    await session.authenticate('authenticator:wallet', { wallet, password });
+    const flashMessages = this.controllerFor('wallets').get('flashMessages');
+    const message = this.get('intl').t('wallets.settings.passwordChanged');
+    flashMessages.success(message);
+    return this.transitionTo('wallets.overview');
   },
 });
