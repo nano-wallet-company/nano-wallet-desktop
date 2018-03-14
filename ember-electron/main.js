b399c005da09c0da9cdf7e91be96b10ffc4f164b
Devin Alexander Torres <d@devinus.io>
2018-03-13 22:27:34 -0700

    fix(electron): re-add menu

--- ember-electron/main.js
+++ ember-electron/main.js
@@ -25,6 +25,7 @@ const toExecutableName = require('to-executable-name');
 const {
   app,
   protocol,
+  Menu,
   BrowserWindow,
   ipcMain,
 } = require('electron');
@@ -284,6 +285,32 @@ const run = async () => {
       return pathExists.sync(databasePath);
     },
   });
+
+  const template = [
+    {
+      label: 'Application',
+      submenu: [
+        { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+        { type: 'separator' },
+        { label: 'Quit', accelerator: 'Command+Q', click() { app.quit(); } },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   mainWindow = new BrowserWindow({
     tabbingIdentifier,
