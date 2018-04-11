694737bf896b2353ebce0eaae1f7e76f3c2a77e9
Devin Alexander Torres <d@devinus.io>
2018-04-10 22:27:55 -0500

    refactor(download-progress): use registerDisposable to cleanup events

--- app/components/download-progress/component.js
+++ app/components/download-progress/component.js
@@ -1,9 +1,13 @@
 import Component from '@ember/component';
 
-import { runTask, debounceTask, runDisposables } from 'ember-lifeline';
-import { on } from 'ember-decorators/object/evented';
-
+import {
+  runTask,
+  debounceTask,
+  runDisposables,
+  registerDisposable,
+} from 'ember-lifeline';
 import { defineError } from 'ember-exex/error';
+import { on } from 'ember-decorators/object/evented';
 
 export const STATUS_DOWNLOADING = 'downloading';
 export const STATUS_VERIFYING = 'verifying';
@@ -23,31 +27,32 @@ export default Component.extend({
 
   onDone: null,
 
-  @on('init')
-  addListeners() {
-    const downloader = this.get('downloader');
-    downloader.on('error', this, this.onError);
-    downloader.on('progress', this, this.onProgress);
-    downloader.on('verify', this, this.onVerify);
-    downloader.on('extract', this, this.onExtract);
-    downloader.on('done', this, this.reset);
-    downloader.on('done', this, this.onDone);
-  },
-
-  @on('willDestroyElement')
-  removeListeners() {
-    const downloader = this.get('downloader');
-    downloader.off('error', this, this.onError);
-    downloader.off('progress', this, this.onProgress);
-    downloader.off('verify', this, this.onVerify);
-    downloader.off('extract', this, this.onExtract);
-    downloader.off('done', this, this.reset);
-    downloader.off('done', this, this.onDone);
-  },
-
   willDestroy(...args) {
     runDisposables(this);
     return this._super(...args);
+  },
+
+  @on('didInsertElement')
+  addListeners() {
+    registerDisposable(this, () => {
+      const downloader = this.get('downloader');
+      downloader.off('error', this, this.onError);
+      downloader.off('progress', this, this.onProgress);
+      downloader.off('verify', this, this.onVerify);
+      downloader.off('extract', this, this.onExtract);
+      downloader.off('done', this, this.reset);
+      downloader.off('done', this, this.onDone);
+    });
+
+    return runTask(this, () => {
+      const downloader = this.get('downloader');
+      downloader.on('error', this, this.onError);
+      downloader.on('progress', this, this.onProgress);
+      downloader.on('verify', this, this.onVerify);
+      downloader.on('extract', this, this.onExtract);
+      downloader.on('done', this, this.reset);
+      downloader.on('done', this, this.onDone);
+    });
   },
 
   onError() {
@@ -56,19 +61,19 @@ export default Component.extend({
   },
 
   onProgress(value) {
-    debounceTask(this, 'updateProgress', value, 250);
+    return debounceTask(this, 'updateProgress', value, 250);
   },
 
   onVerify() {
-    this.reset(STATUS_VERIFYING);
+    return this.reset(STATUS_VERIFYING);
   },
 
   onExtract() {
-    this.reset(STATUS_EXTRACTING);
+    return this.reset(STATUS_EXTRACTING);
   },
 
   reset(status = STATUS_DOWNLOADING) {
-    runTask(this, () => {
+    return runTask(this, () => {
       this.setProperties({
         status,
         value: 1,
