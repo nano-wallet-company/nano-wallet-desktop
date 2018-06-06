1f38a58961e05f6cffaaf1fb7f49348de99a15b7
Devin Alexander Torres <d@devinus.io>
2018-06-05 23:45:54 -0500

    build(electron): only target Chrome when building for Electron

--- config/targets.js
+++ config/targets.js
@@ -1,15 +1,17 @@
 /* eslint-env node */
-const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions',
-];
+const browsers = ['last 1 Chrome versions'];
+const isElectron = !!process.env.EMBER_CLI_ELECTRON;
+if (!isElectron) {
+  browsers.push(
+    'last 1 Firefox versions',
+    'last 1 Safari versions',
+  );
 
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
+  const isCI = !!process.env.CI;
+  const isProduction = process.env.EMBER_ENV === 'production';
+  if (isCI || isProduction) {
+    browsers.push('ie 11');
+  }
 }
 
 module.exports = {
