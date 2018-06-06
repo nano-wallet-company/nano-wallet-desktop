66011743a3bf903fea80bb651e7fed937888c48f
Devin Alexander Torres <d@devinus.io>
2018-06-05 23:44:45 -0500

    refactor(electron): simplify hostManager host when in Electron context

--- config/hosts.js
+++ config/hosts.js
@@ -1,40 +1,34 @@
 /* eslint-env node */
 
-// eslint-disable-next-line arrow-body-style
-module.exports = (/* environment */) => {
-  if (process.env.EMBER_CLI_ELECTRON) {
-    return {
-      default: {
-        rpcHost: 'https://localhost:17076',
-        rpcNamespace: 'rpc',
-      },
-    };
-  }
+module.exports = (/* environment */) => ({
+  /*
+    If present, the host configuration will be pulled based on the given
+    string.
 
-  return {
-    /*
-      If present, the host configuration will be pulled based on the given
-      string.
+    ex) `APP_HOST=myapp.com ember s`
+    */
+  // hostOverride: process.env.APP_HOST,
 
-      ex) `APP_HOST=myapp.com ember s`
-     */
-    // hostOverride: process.env.APP_HOST,
+  /*
+    If the host doesn't resolve to any of the defined configs here,
+    this will be the config used
+    */
+  default: {
+    rpcHost: 'http://localhost:55000',
+    rpcNamespace: null,
+  },
 
-    /*
-      If the host doesn't resolve to any of the defined configs here,
-      this will be the config used
-     */
-    default: {
-      rpcHost: 'http://localhost:55000',
-      rpcNamespace: null,
-    },
+  /*
+    This is the host config that will be used in the test environment
+    */
+  test: {
+    rpcHost: '',
+    rpcNamespace: 'rpc',
+  },
 
-    /*
-      This is the host config that will be used in the test environment
-     */
-    test: {
-      rpcHost: '',
-      rpcNamespace: 'rpc',
-    },
-  };
-};
+  // Electron
+  dist: {
+    rpcHost: 'https://localhost:17076',
+    rpcNamespace: 'rpc',
+  },
+});
