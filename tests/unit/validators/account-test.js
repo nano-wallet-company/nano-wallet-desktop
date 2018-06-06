78c9206156ca9b8dbfb6f5c26d2f48bbcb59daba
Devin Alexander Torres <d@devinus.io>
2018-06-05 23:47:23 -0500

    test(validators): add several tests for our ember-changeset validators

--- tests/unit/validators/account-test.js
+++ tests/unit/validators/account-test.js
@@ -3,9 +3,29 @@ import { describe, it } from 'mocha';
 import validateAccount from '@nanocurrency/nano-desktop/validators/account';
 
 describe('Unit | Validator | account', () => {
-  // Replace this with your real tests.
-  it('exists', () => {
-    const result = validateAccount();
-    expect(result).to.be.ok;
+  it('passes addresses with xrb_ prefix', () => {
+    const validator = validateAccount();
+    const account = 'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4';
+    expect(validator('account', account)).to.be.true;
+  });
+
+  it('passes addresses with nano_ prefix', () => {
+    const validator = validateAccount();
+    const account = 'nano_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4';
+    expect(validator('account', account)).to.be.true;
+  });
+
+  it('fails invalid addresses', () => {
+    const validator = validateAccount();
+    const values = [
+      'xrp_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4',
+      'xrb_4arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4',
+      'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjpsl',
+      'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjpsv',
+      'xrb_arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4',
+      'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps44',
+    ];
+
+    values.forEach(v => expect(validator('account', v)).to.not.be.true);
   });
 });
