9f5b279b251168f85609f5cf135953ec185181d1
Devin Alexander Torres <d@devinus.io>
2018-06-05 23:34:07 -0500

    feat(status-tooltip): visually distinguish when syncing

--- app/components/status-tooltip/component.js
+++ app/components/status-tooltip/component.js
@@ -4,9 +4,13 @@ import InViewportMixin from 'ember-in-viewport';
 
 import { service } from 'ember-decorators/service';
 import { on } from 'ember-decorators/object/evented';
+import { gt, lt } from 'ember-decorators/object/computed';
 
 export default Component.extend(InViewportMixin, {
   @service status: null,
+
+  @gt('status.blocks.unchecked', 0) hasUncheckedBlocks: false,
+  @lt('status.peers.length', 1) isPeerless: false,
 
   @on('didEnterViewport')
   startPolling() {
