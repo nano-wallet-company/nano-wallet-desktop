e337d343170d9737d38a8248d40714259991fbc1
Devin Alexander Torres <d@devinus.io>
2018-06-17 09:27:07 -0500

    test(app): fix usage of ember-test-helpers

--- tests/integration/components/account-address/component-test.js
+++ tests/integration/components/account-address/component-test.js
@@ -1,15 +1,13 @@
-import { find } from '@ember/test-helpers';
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { setupComponentTest } from 'ember-mocha';
+import { it, describe } from 'mocha';
+import { setupRenderingTest } from 'ember-mocha';
+import { find, findAll, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 describe('Integration | Component | account-address', () => {
-  setupComponentTest('account-address', {
-    integration: true,
-  });
+  setupRenderingTest();
 
-  it('renders', function () {
+  it('renders', async () => {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
     // Template block usage:
@@ -19,28 +17,28 @@ describe('Integration | Component | account-address', () => {
     //   {{/account-address}}
     // `);
 
-    this.render(hbs`{{account-address}}`);
-    expect(this.$()).to.have.length(1);
+    await render(hbs`{{account-address}}`);
+    expect(find('span')).to.exist;
   });
 
-  it('displays the full address', function () {
+  it('displays the full address', async function () {
     const value = 'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4';
     this.set('value', value);
-    this.render(hbs`{{account-address value=value}}`);
+    await render(hbs`{{account-address value=value}}`);
 
-    const address = find('*').textContent.trim();
+    const address = this.element.textContent.trim();
     expect(address).to.equal(value);
   });
 
-  it('visually separates the first 9 and last 5 characters', function () {
+  it('visually separates the first 9 and last 5 characters', async function () {
     const value = 'xrb_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4';
     this.set('value', value);
-    this.render(hbs`{{account-address value=value}}`);
+    await render(hbs`{{account-address value=value}}`);
 
-    const parts = this.$('span > span');
+    const parts = findAll('span > span');
     expect(parts).to.have.length(3);
 
-    const [head, body, tail] = parts.map((idx, el) => el.innerText.trim());
+    const [head, body, tail] = parts.map(el => el.innerText.trim());
     expect(head).to.equal('xrb_3arg3');
     expect(body).to.equal('asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6q');
     expect(tail).to.equal('gjps4');
