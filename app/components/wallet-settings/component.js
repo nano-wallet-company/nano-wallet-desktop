771cabbea5839b906a719ecd0d85591e9b9c8906
kennedybaird <kennedybaird@gmail.com>
2018-06-30 15:35:36 +0700

    refactor(settings): change style of new settings modal

--- app/components/wallet-settings/component.js
+++ app/components/wallet-settings/component.js
@@ -2,10 +2,12 @@ import Component from '@ember/component';
 
 import { storageFor } from 'ember-local-storage';
 
-import { readOnly } from 'ember-decorators/object';
+import { action, readOnly } from 'ember-decorators/object';
 import { alias } from 'ember-decorators/object/computed';
 
+import downloadjs from 'npm:downloadjs';
 import ChangeRepresentativeValidations from '../../validations/change-representative';
+
 
 export default Component.extend({
   ChangeRepresentativeValidations,
@@ -25,4 +27,17 @@ export default Component.extend({
   revealSeed: null,
   changeRep: null,
   changePass: null,
+  revealMnemonic: null,
+
+  @action
+  copyMnemonic() {
+    const message = this.get('intl').t('wallets.backup.copied');
+    this.get('flashMessages').success(message);
+  },
+
+  @action
+  downloadMnemonic(mnemonic) {
+    const fileName = String(mnemonic).split(' ').slice(0, 2).join('-');
+    downloadjs(mnemonic, fileName, 'text/plain');
+  },
 });
