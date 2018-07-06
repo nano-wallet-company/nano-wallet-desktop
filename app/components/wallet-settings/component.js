2c5df696fe347fb9208dddf36e3b0910233cb4c8
kennedybaird <kennedybaird@gmail.com>
2018-07-06 12:38:33 +0700

    refactor(settings): settings toggle, new translations

--- app/components/wallet-settings/component.js
+++ app/components/wallet-settings/component.js
@@ -2,10 +2,9 @@ import Component from '@ember/component';
 
 import { storageFor } from 'ember-local-storage';
 
-import { action, readOnly } from 'ember-decorators/object';
+import { computed, action, readOnly } from 'ember-decorators/object';
 import { alias } from 'ember-decorators/object/computed';
 
-import downloadjs from 'npm:downloadjs';
 import ChangeRepresentativeValidations from '../../validations/change-representative';
 
 
@@ -24,20 +23,24 @@ export default Component.extend({
   onChangeRepresentative: null,
   onChangePassword: null,
 
-  revealSeed: null,
-  changeRep: null,
-  changePass: null,
-  revealMnemonic: null,
-
-  @action
-  copyMnemonic() {
-    const message = this.get('intl').t('wallets.backup.copied');
-    this.get('flashMessages').success(message);
+  @computed
+  get items() {
+    return {
+      revealSeed: null,
+      changeRep: null,
+      changePass: null,
+    };
   },
 
   @action
-  downloadMnemonic(mnemonic) {
-    const fileName = String(mnemonic).split(' ').slice(0, 2).join('-');
-    downloadjs(mnemonic, fileName, 'text/plain');
+  toggleItems(current) {
+    const items = this.get('items');
+    Object.keys(items).forEach((key) => {
+      if (key === current) {
+        this.toggleProperty(`items.${key}`);
+      } else {
+        this.set(`items.${key}`, null);
+      }
+    });
   },
 });
