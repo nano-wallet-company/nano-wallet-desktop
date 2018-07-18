5d79b3684ef760359d072cdfef8c8b95bd893b3f
kennedybaird <kennedybaird@gmail.com>
2018-07-18 15:47:27 +0700

    refactor(settings): update settings modal ui + include seed qr code

--- app/components/wallet-settings/component.js
+++ app/components/wallet-settings/component.js
@@ -2,10 +2,11 @@ import Component from '@ember/component';
 
 import { storageFor } from 'ember-local-storage';
 
-import { readOnly } from 'ember-decorators/object';
+import { computed, action, readOnly } from 'ember-decorators/object';
 import { alias } from 'ember-decorators/object/computed';
 
 import ChangeRepresentativeValidations from '../../validations/change-representative';
+
 
 export default Component.extend({
   ChangeRepresentativeValidations,
@@ -21,4 +22,25 @@ export default Component.extend({
 
   onChangeRepresentative: null,
   onChangePassword: null,
+
+  @computed
+  get items() {
+    return {
+      revealSeed: null,
+      changeRep: null,
+      changePass: null,
+    };
+  },
+
+  @action
+  toggleItems(current) {
+    const items = this.get('items');
+    Object.keys(items).forEach((key) => {
+      if (key === current) {
+        this.toggleProperty(`items.${key}`);
+      } else {
+        this.set(`items.${key}`, null);
+      }
+    });
+  },
 });
