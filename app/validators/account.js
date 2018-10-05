ea8306ac2d4da4c3339d46defbb7cde321da0af9
catenocrypt <catenocrypt@users.noreply.github.com>
2018-10-05 08:44:44 +0200

    Change account validation rules to accept mik_.

--- app/validators/account.js
+++ app/validators/account.js
@@ -1,6 +1,6 @@
 import { validateFormat } from 'ember-changeset-validations/validators';
 
-export const regex = /^(xrb|nano)(_|-)[13]((?![lv])[a-z1-9]){59}$/;
+export const regex = /^(mik|xrb)(_|-)[13]((?![lv])[a-z1-9]){59}$/;
 
 export default function validateAccount(options = {}) {
   return validateFormat(Object.assign({}, options, { regex }));
