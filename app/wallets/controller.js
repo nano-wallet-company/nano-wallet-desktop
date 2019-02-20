3444535aebc6ea546a889c5a0e69cee93d94da5b
Devin Alexander Torres <d@devinus.io>
2019-02-19 23:02:34 -0600

    perf(wallets-controller): use wallet_balances over accounts_balances

--- app/wallets/controller.js
+++ app/wallets/controller.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
 import { get } from '@ember/object';
-import { A } from '@ember/array';
-import { isEmpty } from '@ember/utils';
 
 import { ContextBoundTasksMixin } from 'ember-lifeline';
 import { inject as service } from '@ember-decorators/service';
@@ -38,20 +36,16 @@ export default class WalletsController extends Controller.extend(
     });
   }
 
-  async updateBalances(wallet) {
+  async updateBalances(model) {
     const rpc = this.get('rpc');
-    await rpc.searchPending(get(wallet, 'id'));
+    const wallet = get(model, 'id');
+    const balances = await rpc.walletBalances(wallet);
+    const data = Object.entries(balances).map(([id, attributes]) => ({
+      id,
+      attributes,
+      type: 'account',
+    }));
 
-    const accounts = A(get(wallet, 'accounts')).map(acc => get(acc, 'id'));
-    if (!isEmpty(accounts)) {
-      const balances = await rpc.accountsBalances(accounts);
-      const data = Object.entries(balances).map(([id, attributes]) => ({
-        id,
-        attributes,
-        type: 'account',
-      }));
-
-      this.get('store').push({ data });
-    }
+    this.get('store').push({ data });
   }
 }
